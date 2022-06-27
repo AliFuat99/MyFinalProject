db305e67e5a6c03ca14a53902b39bb3ad867cc95
Alifuat99 <alifuatbudanmis29@gmail.com>
2022-06-27 08:27:24 +0300

    MyFinalProject

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    //static sürekli new kullanmamak için var. Tek bir instance
+    //public yapılar büyük yazılır pascal keys
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalıd = "Ürün İsmi Geçersiz";
+        internal static string MaintenanceTime = "Sistem Bakımda";
+        internal static string ProductsListed = "Ürünler Listelendi";
+        internal static string ProductCountOfCategoryError;
+    }
+}
